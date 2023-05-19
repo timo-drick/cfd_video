3fd396deaa3b37307118c2511b0d480256c66134
Timo Drick <t.drick@apps-on-air.de>
2023-05-19 14:12:58 +0200

    Cleanups and dependency updates

--- Compose for Desktop Video Player/build.gradle.kts
+++ Compose for Desktop Video Player/build.gradle.kts
@@ -1,12 +1,10 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-
 plugins {
     kotlin("multiplatform") version Version.kotlin
     id("org.jetbrains.compose") version Version.compose
     id("com.github.ben-manes.versions") version "0.46.0"
 }
 
-group = "de.appsonair.compose"
+group = Group.groupId
 version = "1.0-SNAPSHOT"
 
 allprojects {
@@ -24,3 +22,15 @@ kotlin {
     }
 }
 
+fun isNonStable(version: String): Boolean {
+    val unStableKeyword = listOf("alpha", "beta", "rc", "cr", "m", "preview", "dev").any { version.contains(it, ignoreCase = true) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = unStableKeyword.not() || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.named("dependencyUpdates", com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask::class.java).configure {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+}
--- Compose for Desktop Video Player/buildSrc/src/main/kotlin/versions.kt
+++ Compose for Desktop Video Player/buildSrc/src/main/kotlin/versions.kt
@@ -1,7 +1,7 @@
 object Version {
     const val jvmTarget = "11"
-    const val kotlin = "1.8.0"
-    const val compose = "1.3.0"
+    const val kotlin = "1.8.20"
+    const val compose = "1.4.0"
 }
 
 object Group {
